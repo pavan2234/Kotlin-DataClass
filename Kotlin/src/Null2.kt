a19c928a617ed407310b9b4bfe11f5e009b93bbe
Ananthoju Pavan <pavan.a@techouts.com>
2022-05-12 11:38:29 +0530

    null example

---
+++ Kotlin/src/Null2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var s:String = "Hello"
+    //s =null
+    //Here s is a nonnull variable so if comd will be false
+    var len = if(s!=null) s.length else -1
+    println(len)
+
+    var a:String?="Hello there"
+    a = null
+    return if(a!=null) println("a is not a nullvalue $a") else println("a is null value $a")
+}
